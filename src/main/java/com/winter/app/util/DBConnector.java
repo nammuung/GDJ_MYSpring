c6cd49b1662355d88f34c8e22efe98d425c20594
nammuung <audrbs2368@naver.com>
2024-01-08 14:12:49 +0900

    add

--- src/main/java/com/winter/app/util/DBConnector.java
+++ src/main/java/com/winter/app/util/DBConnector.java
@@ -12,14 +12,13 @@ public class DBConnector {
 	public static Connection getConnector() throws Exception {
 		String user="user02";
 		String password="user02";
-		String url="jdbc:oracle:thin:@54.180.88.202:1521:xe";
+		String url="jdbc:oracle:thin:@43.201.26.210:1521:xe";
 		String driver="oracle.jdbc.driver.OracleDriver";
 		
 		//1. driver를 메모리에 로딩(객체 생성)
 		Class.forName(driver);
 		//2. DB연결
 		Connection con = DriverManager.getConnection(url, user, password);
-		
 		return con;
 		
 	}
